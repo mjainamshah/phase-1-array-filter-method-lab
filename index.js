dd66be7c44b9f33c3654aa97708d758e738c7bc9
mjainamshah <jainam.shah@student.moringaschool.com>
2023-09-26 09:59:10 +0300

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers , name) {
+    const lowercaseName = name.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+      const lowercaseDriverName = driver.toLowerCase();
+      return lowercaseDriverName.includes(lowercaseName);
+    });
+  
+    return matchingDrivers;
+  }
+
+
+  function fuzzyMatch (drivers , searchText) {
+      return drivers.filter (driver => driver.startsWith(searchText));
+  }
+  
+  function matchName (drivers, name) {
+    return drivers.filter (drivers => drivers.name === name);
+  }
